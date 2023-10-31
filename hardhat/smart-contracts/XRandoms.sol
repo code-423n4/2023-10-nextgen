dfec6be66f82f2f696fbed0731df25cec085f2e5
Kambiz Asadzadeh <kambiz.ceo@gmail.com>
2023-10-31 17:07:42 +0330

    Optimization and enhancement.
    - Optimized and Enhanced Word Retrieval Function.

--- hardhat/smart-contracts/XRandoms.sol
+++ hardhat/smart-contracts/XRandoms.sol
@@ -12,10 +12,8 @@ pragma solidity ^0.8.19;
 
 contract randomPool {
 
-    function getWord(uint256 id) private pure returns (string memory) {
-        
-        // array storing the words list
-        string[100] memory wordsList = ["Acai", "Ackee", "Apple", "Apricot", "Avocado", "Babaco", "Banana", "Bilberry", "Blackberry", "Blackcurrant", "Blood Orange", 
+    // wordsList is a pre-defined array containing 100 unique words.
+    string[100] private wordsList = ["Acai", "Ackee", "Apple", "Apricot", "Avocado", "Babaco", "Banana", "Bilberry", "Blackberry", "Blackcurrant", "Blood Orange", 
         "Blueberry", "Boysenberry", "Breadfruit", "Brush Cherry", "Canary Melon", "Cantaloupe", "Carambola", "Casaba Melon", "Cherimoya", "Cherry", "Clementine", 
         "Cloudberry", "Coconut", "Cranberry", "Crenshaw Melon", "Cucumber", "Currant", "Curry Berry", "Custard Apple", "Damson Plum", "Date", "Dragonfruit", "Durian", 
         "Eggplant", "Elderberry", "Feijoa", "Finger Lime", "Fig", "Gooseberry", "Grapes", "Grapefruit", "Guava", "Honeydew Melon", "Huckleberry", "Italian Prune Plum", 
@@ -24,13 +22,10 @@ contract randomPool {
         "Plantain", "Plum", "Pomegranate", "Pomelo", "Prickly Pear", "Pulasan", "Quine", "Rambutan", "Raspberries", "Rhubarb", "Rose Apple", "Sapodilla", "Satsuma", 
         "Soursop", "Star Apple", "Star Fruit", "Strawberry", "Sugar Apple", "Tamarillo", "Tamarind", "Tangelo", "Tangerine", "Ugli", "Velvet Apple", "Watermelon"];
 
-        // returns a word based on index
-        if (id==0) {
-            return wordsList[id];
-        } else {
-            return wordsList[id - 1];
-        }
-        }
+    function getWord(uint256 id) public view returns (string memory) {
+        require(id > 0 && id <= wordsList.length, "Invalid word ID");
+        return wordsList[id - 1];
+    }
 
     function randomNumber() public view returns (uint256){
         uint256 randomNum = uint(keccak256(abi.encodePacked(block.prevrandao, blockhash(block.number - 1), block.timestamp))) % 1000;
